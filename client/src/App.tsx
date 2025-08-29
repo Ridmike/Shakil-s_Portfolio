cd52689e6cfa56d058dd4417a52b0a941f8868c8
Shakil Ridmike <ridmikew@gmail.com>
2025-08-30 03:17:15 +0530

    Fix routing and deployment configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,20 +5,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Navigation } from "@/components/navigation";
 import Home from "@/pages/home";
-import NotFound from "@/pages/not-found";
-
-const base = '/Shakil-s_Portfolio';
-
-function Router() {
-  return (
-    <WouterRouter base={base}>
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-    </WouterRouter>
-  );
-}
 
 function App() {
   return (
@@ -28,7 +13,7 @@ function App() {
         <TooltipProvider>
           <Navigation />
           <Toaster />
-          <Router />
+          <Home />
         </TooltipProvider>
       </ThemeProvider>
     </QueryClientProvider>
