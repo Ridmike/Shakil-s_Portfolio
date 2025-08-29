0661668aa55c2c8c38bbc655d27f600df39d1cd3
Shakil Ridmike <ridmikew@gmail.com>
2025-08-30 03:13:32 +0530

    Fix GitHub Pages routing and build configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
@@ -8,12 +8,16 @@ import { Navigation } from "@/components/navigation";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
 
+const base = '/Shakil-s_Portfolio';
+
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base={base}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
